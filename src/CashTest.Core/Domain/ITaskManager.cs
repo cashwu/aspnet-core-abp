466a57175d6c2e45ae6f0c10360b5ac7a2b5193e
Cash Wu <cash@cashwu.com>
2018-04-12 23:23:45 +0800

    ddd

---
+++ src/CashTest.Core/Domain/ITaskManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Services;
+using CashTest.Entities;
+
+namespace CashTest.Domain
+{
+    public interface ITaskManager : IDomainService
+    {
+        void AssignTaskToPerson(Task task, Player player);
+    }
+}
