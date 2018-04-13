9d0e6bd44a5e253b7ca5b8fc59088f9ae311ddba
Cash Wu <cash@cashwu.com>
2018-04-13 17:09:15 +0800

    dto validation

--- src/CashTest.Application/Player/Dto/PlayerInput.cs
+++ src/CashTest.Application/Player/Dto/PlayerInput.cs
@@ -7,11 +7,25 @@ using Abp.Runtime.Validation;
 
 namespace CashTest.Player.Dto
 {
-    public class PlayerInput
+    public class PlayerInput : ICustomValidate, IShouldNormalize
     {
         public long PlayerID { get; set; }
         public string PlayerName { get; set; }
         public long MapID { get; set; }
+
+        public void AddValidationErrors(CustomValidationContext context)
+        {
+            context.Results.Add(ValidationResult.Success);
+            //context.Results.Add(new ValidationResult("error"));
+        }
+
+        public void Normalize()
+        {
+            if (string.IsNullOrEmpty(PlayerName))
+            {
+                PlayerName = "cash";
+            }   
+        }
     }
 
     public class GetPlayerInput
